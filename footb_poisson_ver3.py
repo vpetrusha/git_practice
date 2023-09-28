64d1983b2f9a591c91391c7b5007c171421cf6a4
vpetrusha <petrushavictor@gmail.com>
2023-09-28 10:58:09 +0300

    With function now implemented

--- footb_poisson_ver3.py
+++ footb_poisson_ver3.py
@@ -10,16 +10,13 @@ ratio = 4
 dice_roll_home = (home_team + form_home + ratio) 
 dice_roll_away = (away_team + form_away + ratio)
 
-list_home = []
+def dice_roll():
+	dice_roll_home_list = []
+	dice_roll_away_list = []
+	for _ in range(dice_roll_home, dice_roll_away):
+		dice_roll_home_list.append(random.randint(1, 6))
+		dice_roll_away_list.append(random.randint(1,6))
+	print(f"The probable score is {dice_roll_home_list.count(6)} : {dice_roll_away_list.count(6)}")
 
-for _ in range(dice_roll_home):
-	list_home.append(random.randint(1, 6))
-print(list_home)
 
-list_away = []
-
-for _ in range(dice_roll_away):
-	list_away.append(random.randint(1, 6))
-print(list_away)
-
-print(f"The probable score is {list_home.count(6)} : {list_away.count(6)}")
+dice_roll()
